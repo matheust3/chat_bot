2329b3558efcb7330ed0b6d82ce4493111cf129b
matheus toniolli <matheus.toniolli@hotmail.com>
2021-01-31 19:04:24 +0000

    chore: create static sticker

---
+++ src/infra/create-static-sticker-datasource-impl.spec.ts
@@ -0,0 +1,71 @@
+import { CreateStaticStickerDatasourceImpl } from './create-static-sticker-datasource-impl'
+import fs from 'fs'
+
+jest.mock('uuid', () => ({
+  v4: (): string => 'uId'
+}))
+
+interface SutTypes{
+  datasource: CreateStaticStickerDatasourceImpl
+}
+
+const makeSut = (): SutTypes => {
+  const datasource = new CreateStaticStickerDatasourceImpl()
+  return { datasource }
+}
+
+jest.mock('fs')
+const execFunc = jest.fn().mockResolvedValue({ stdout: 'ok' })
+jest.mock('util', () => ({
+  promisify: jest.fn(() => {
+    return execFunc
+  })
+}))
+
+describe('CreateStaticStickerDatasourceImpl', () => {
+  test('ensure check if cache exists', async () => {
+    //! Arrange
+    const { datasource } = makeSut()
+    //! Act
+    await datasource.createSticker(Buffer.from('any buffer'))
+    //! Assert
+    expect(fs.existsSync).toHaveBeenCalledWith(`${__dirname}/../cache`)
+  })
+  test('ensure create cache folder if not exists', async () => {
+    //! Arrange
+    const { datasource } = makeSut()
+    jest.spyOn(fs, 'existsSync').mockReturnValue(false)
+    jest.spyOn(fs, 'mkdirSync')
+    //! Act
+    await datasource.createSticker(Buffer.from('any buffer'))
+    //! Assert
+    expect(fs.mkdirSync).toHaveBeenCalledWith(`${__dirname}/../cache`)
+  })
+  test('ensure save file in cache folder to convert to png image', async () => {
+    //! Arrange
+    const { datasource } = makeSut()
+    const buffer = Buffer.from('any buffer')
+    //! Act
+    await datasource.createSticker(buffer)
+    //! Assert
+    expect(fs.writeFileSync).toHaveBeenCalledWith('uId', buffer)
+  })
+  test('ensure convert file to png', async () => {
+    //! Arrange
+    const { datasource } = makeSut()
+    //! Act
+    await datasource.createSticker(Buffer.from('any buffer'))
+    //! Assert
+    expect(execFunc).toHaveBeenCalledWith(`mogrify -format png ${__dirname}/../cache/uId`)
+  })
+  test('ensure return new file buffer', async () => {
+    //! Arrange
+    const { datasource } = makeSut()
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(Buffer.from('new buffer'))
+    //! Act
+    const result = await datasource.createSticker(Buffer.from('any buffer'))
+    //! Assert
+    expect(fs.readFileSync).toHaveBeenCalledWith(`${__dirname}/../cache/uId.png`)
+    expect(result).toEqual(Buffer.from('new buffer'))
+  })
+})
---
+++ src/infra/create-static-sticker-datasource-impl.ts
@@ -0,0 +1,21 @@
+import { CreateStaticStickerDatasource } from '../data/datasources/create-static-sticker-datasource'
+import fs from 'fs'
+import { v4 } from 'uuid'
+import { promisify } from 'util'
+import child_process from 'child_process'
+
+export class CreateStaticStickerDatasourceImpl implements CreateStaticStickerDatasource {
+  async createSticker (data: Buffer): Promise<Buffer> {
+    if (!fs.existsSync(`${__dirname}/../cache`)) {
+      fs.mkdirSync(`${__dirname}/../cache`)
+    }
+    const uuid = v4()
+    fs.writeFileSync(uuid, data)
+
+    const exec = promisify(child_process.exec)
+    const { stdout, stderr } = await exec(`mogrify -format png ${__dirname}/../cache/${uuid}`)
+    console.log('out: ', stdout)
+    console.log('err: ', stderr)
+    return fs.readFileSync(`${__dirname}/../cache/${uuid}.png`)
+  }
+}
