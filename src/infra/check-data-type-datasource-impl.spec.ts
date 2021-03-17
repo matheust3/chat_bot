1f220f0ca91785f71c77e38cd8b10e389ae8ca5d
matheus toniolli <matheus.toniolli@hotmail.com>
2021-03-17 10:54:09 -0400

    chore: webp is unsupported

--- src/infra/check-data-type-datasource-impl.spec.ts
+++ src/infra/check-data-type-datasource-impl.spec.ts
@@ -60,15 +60,7 @@ describe('CheckDataTypeDatasourceImpl', () => {
     //! Assert
     expect(result).toBe('stickerAnimated' as DataType)
   })
-  test('ensure return animatedSticker if data is webp', async () => {
-    //! Arrange
-    const { datasource } = makeSut()
-    const buffer = fs.readFileSync(`${__dirname}/../../fixtures/webp.webp`)
-    //! Act
-    const result = await datasource.fromBuffer(buffer)
-    //! Assert
-    expect(result).toBe('stickerAnimated' as DataType)
-  })
+
   test('ensure return invalid if data is not a static image and not a animated/video', async () => {
     //! Arrange
     const { datasource } = makeSut()
