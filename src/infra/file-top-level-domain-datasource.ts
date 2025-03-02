9f0bb715d82071f8ddcde9bced244c54ed976dac
Matheus Toniolli <matheus.toniolli@hotmail.com>
2025-03-02 13:50:46 +0000

    fix: simplify file path handling in FileTopLevelDomainDatasource constructor

--- src/infra/file-top-level-domain-datasource.ts
+++ src/infra/file-top-level-domain-datasource.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import { ITopLevelDomainDatasource } from '../data/datasources/top-level-domain-datasource'
 import fs from 'fs'
 
@@ -7,7 +6,7 @@ export class FileTopLevelDomainDatasource implements ITopLevelDomainDatasource {
 
   constructor (filePath: string) {
     // Lê o arquivo com a lista de domínios de primeiro nível
-    const file = fs.readFileSync(path.join(__dirname, filePath))
+    const file = fs.readFileSync(filePath)
     // Converte o arquivo em uma lista de domínios
     this.topLevelDomains = file.toString().toLocaleLowerCase().split('\n')
   }
