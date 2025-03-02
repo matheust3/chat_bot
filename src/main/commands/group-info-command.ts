41a4b8ce41b58704cc732143fab33007706d3e47
Matheus Toniolli <matheus.toniolli@hotmail.com>
2025-03-02 12:40:22 +0000

    chore: avisa que o comando necessita de argumentos

--- src/main/commands/group-info-command.ts
+++ src/main/commands/group-info-command.ts
@@ -7,9 +7,13 @@ export default async (message: IMessage, client: IClient): Promise<void> => {
       if (message.groupId === undefined) {
         await client.sendText(message.from, 'Esse comando só pode ser usado em grupos', { quotedMsg: message.id })
       } else {
-        if (message.command.args?.includes('id') === true) {
-          const groupId = message.groupId
-          await client.sendText(message.groupId ?? message.from, groupId, { quotedMsg: message.id })
+        if (message.command.args?.length === 0 || message.command.args === undefined) {
+          await client.sendText(message.groupId, 'É necessário fornecer um argumento para o comanda', { quotedMsg: message.id })
+        } else {
+          if (message.command.args?.includes('id')) {
+            const groupId = message.groupId
+            await client.sendText(message.groupId ?? message.from, groupId, { quotedMsg: message.id })
+          }
         }
       }
     }
