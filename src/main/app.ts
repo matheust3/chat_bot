291b1dd3818e34491cc222add8d4176c3de768d8
matheus toniolli <matheus.toniolli@hotmail.com>
2025-11-06 14:36:38 +0000

    feat: update groq-sdk to version 0.34.0 and improve path handling in tests

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import 'dotenv/config'
 import { create, Message } from '@wppconnect-team/wppconnect'
 import path from 'path'
 import { promisify } from 'util'
@@ -8,8 +8,17 @@ import { messageAdapter } from './adapters/messageAdapter'
 import commands from './config/commands'
 import middlewares from './config/middlewares'
 import { clientAdapter } from './adapters/clientAdapter'
-import { ChatsDatasourceImpl } from '../infra/chats-datasource-impl'
-import { Database } from 'sqlite3'
+
+interface IOriginQuotedMsg {
+  id: {
+    _serialized: string
+  }
+  body: string
+  from: string
+  type: string
+}
+
+type ExtendedMessage = Message & { fromMe?: boolean, caption?: string, quotedMsg?: IOriginQuotedMsg, quotedParticipant: string }
 
 // Apaga os arquivos da pasta 'cache' com mais de um dia
 setInterval(() => {
@@ -30,34 +39,33 @@ create({
   debug: false,
   headless: true,
   browserArgs: ['--no-sandbox']
-}).then((client) => {
+}).then(async (client) => {
   // Recebe a mensagem e envia a resposta
-  client.onAnyMessage((message) => {
-    const msg = messageAdapter(message)
-    const cli = clientAdapter(client)
+  client.onAnyMessage((message: Message) => {
+    void (async () => {
+      const msg = await messageAdapter(message as ExtendedMessage, client)
+      const cli = clientAdapter(client)
 
-              let next = true
-              const nextFunction = (): void => {
-                next = true
-              }
-              // Executa os middlewares
-              for (const middleware of middlewares) {
-                if (!next) break
-                next = false
-                await middleware(msg, cli, nextFunction)
-              }
-              // Bloqueia a execução dos comandos se o middleware retornar false
-              if (!next) return
+      let next = true
+      const nextFunction = (): void => {
+        next = true
+      }
+      // Executa os middlewares
+      const loadedMiddlewares = await middlewares()
+      for (const middleware of loadedMiddlewares) {
+        if (!next) break
+        next = false
+        await middleware(msg, cli, nextFunction)
+      }
+      // Bloqueia a execução dos comandos se o middleware retornar false
+      if (!next) return
 
-              if (msg.isCommand) {
-                for (const command of commands) {
-                  await command(msg, cli)
-                }
-              }
-            })().catch((error) => console.error('Erro ao processar a mensagem', error))
-          })
-        }).catch((error) => console.error('Erro ao criar o cliente', error))
-      }).catch((error) => console.error('Erro ao carregar os comandos', error))
-    }).catch((error) => console.error('Erro ao carregar os middlewares', error))
-  }).catch((error) => console.error('Erro ao criar as tabelas no datasource', error))
-}).catch((error) => console.error('Erro ao liberar a conexão com o banco de dados', error))
+      if (msg.isCommand) {
+        const loadedCommands = await commands()
+        for (const command of loadedCommands) {
+          await command(msg, cli)
+        }
+      }
+    })()
+  })
+}).catch((error) => console.error('Erro ao criar o cliente', error))
