976eb62b5f6cd51e485a05f889c6f301ffb68820
matheus toniolli <matheus.toniolli@hotmail.com>
2025-11-06 23:33:33 +0000

    fix: add bot admin check in ban command to ensure proper permissions

--- src/main/commands/ban-command.ts
+++ src/main/commands/ban-command.ts
@@ -7,6 +7,10 @@ export default async (message: IMessage, client: IClient): Promise<void> => {
       if (message.groupId === undefined) {
         await client.sendText(message.from, 'Esse comando só pode ser usado em grupos', { quotedMsg: message.id })
       } else {
+        if (!await client.botIsAdmin(message.groupId)) {
+          await client.sendText(message.groupId, 'Eu preciso ser administrador para executar esse comando', { quotedMsg: message.id })
+          return
+        }
         const quotedMsgId = message.quotedMsgId
         if (quotedMsgId === undefined) {
           await client.sendText(message.groupId, 'Você precisa responder a mensagem de quem deseja banir', { quotedMsg: message.id })
